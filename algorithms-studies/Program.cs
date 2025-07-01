8e3383afea9d82646ab06ac8a7726d1afd910ff6
Carlos Martins <lemos_oliveiraa@hotmail.com>
2025-06-30 21:13:04 -0300

    [sorting] Bubble Sort

--- algorithms-studies/Program.cs
+++ algorithms-studies/Program.cs
@@ -5,3 +5,4 @@ int[] array = [2, 7, 1, 3, 5, 4, 8, 6, 7];
 Bubble25062025.Sort(array).PrintArray();
 Bubble26062025.Sort(array).PrintArray();
 Bubble26062025_2.Sort(array).PrintArray();
+Bubble30062025.Sort(array).PrintArray();
