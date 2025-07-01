1e949f15d68abc70dbf2b14d01643a33349d38a7
Carlos Martins <lemos_oliveiraa@hotmail.com>
2025-06-30 21:33:59 -0300

    [sorting] Selection Sort

--- algorithms-studies/Program.cs
+++ algorithms-studies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using algorithms_studies.Algorithms.Sorting.BubbleSort;
+using algorithms_studies.Algorithms.Sorting.SelectionSort;
 
 int[] array = [2, 7, 1, 3, 5, 4, 8, 6, 7];
 
@@ -6,3 +7,5 @@ Bubble25062025.Sort(array).PrintArray();
 Bubble26062025.Sort(array).PrintArray();
 Bubble26062025_2.Sort(array).PrintArray();
 Bubble30062025.Sort(array).PrintArray();
+
+SelectionSort30062025.Sort(array).PrintArray();
