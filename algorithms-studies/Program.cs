8f59d240e85ea5c8e19e435fd90edc28fd59c302
Carlos Martins <lemos_oliveiraa@hotmail.com>
2025-07-16 20:41:07 -0300

    [chore] Fix the reference problem with the base array
    - Yeah, after the first sorting, all the others was using an already ordered array... So... Yeah

--- algorithms-studies/Program.cs
+++ algorithms-studies/Program.cs
@@ -2,25 +2,25 @@
 using algorithms_studies.Algorithms.Sorting.InsertionSort;
 using algorithms_studies.Algorithms.Sorting.SelectionSort;
 
-int[] array = [2, 7, 1, 3, 5, 4, 8, 6, 7];
+int[] array = [64, 34, 25, 12, 22, 11, 12, 90];
 
 Console.WriteLine("\n___BUBBLE SORT___\n");
 
-Bubble20250625.Sort(array).PrintArray();
-Bubble20250626.Sort(array).PrintArray();
-Bubble20250626_2.Sort(array).PrintArray();
-Bubble20250630.Sort(array).PrintArray();
-Bubble20250702.Sort(array).PrintArray();
-Bubble20250707.Sort(array).PrintArray();
-Bubble20250710.Sort(array).PrintArray();
+Bubble20250625.Sort(array.ToArray()).PrintArray();
+Bubble20250626.Sort(array.ToArray()).PrintArray();
+Bubble20250626_2.Sort(array.ToArray()).PrintArray();
+Bubble20250630.Sort(array.ToArray()).PrintArray();
+Bubble20250702.Sort(array.ToArray()).PrintArray();
+Bubble20250707.Sort(array.ToArray()).PrintArray();
+Bubble20250710.Sort(array.ToArray()).PrintArray();
 
 Console.WriteLine("\n___SELECTION SORT___\n");
 
-SelectionSort20250630.Sort(array).PrintArray();
-SelectionSort20250630_2.Sort(array).PrintArray();
-SelectionSort20250702.Sort(array).PrintArray();
-SelectionSort20250707.Sort(array).PrintArray();
-SelectionSort20250710.Sort(array).PrintArray();
+SelectionSort20250630.Sort(array.ToArray()).PrintArray();
+SelectionSort20250630_2.Sort(array.ToArray()).PrintArray();
+SelectionSort20250702.Sort(array.ToArray()).PrintArray();
+SelectionSort20250707.Sort(array.ToArray()).PrintArray();
+SelectionSort20250710.Sort(array.ToArray()).PrintArray();
 
 Console.WriteLine("\n___INSERTION SORT___\n");
-InsertionSort20250710.Sort(array).PrintArray();
+InsertionSort20250710.Sort(array.ToArray()).PrintArray();
