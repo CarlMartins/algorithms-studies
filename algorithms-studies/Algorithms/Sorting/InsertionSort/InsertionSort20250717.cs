978a68e1ec494309f8335048930c3552fdc86cb3
Carlos Martins <lemos_oliveiraa@hotmail.com>
2025-07-17 19:50:43 -0300

    [sorting] Insertion Sort

---
+++ algorithms-studies/Algorithms/Sorting/InsertionSort/InsertionSort20250717.cs
@@ -0,0 +1,23 @@
+namespace algorithms_studies.Algorithms.Sorting.InsertionSort;
+
+public static class InsertionSort20250717
+{
+    public static int[] Sort(int[] array)
+    {
+        for (var i = 1; i < array.Length; i++)
+        {
+            var key = array[i];
+            var j = i - 1;
+
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+
+        return array;
+    }
+}
