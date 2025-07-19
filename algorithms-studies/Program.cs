d150144177b4a2a6ac57fcb7adc1a309fb602d5c
Carlos Martins <lemos_oliveiraa@hotmail.com>
2025-07-19 20:02:20 -0300

    [sorting] Insertion Sort

--- algorithms-studies/Program.cs
+++ algorithms-studies/Program.cs
@@ -39,3 +39,4 @@ InsertionSort20251707_4.Sort(array.ToArray()).PrintArray();
 InsertionSort20251707_5.Sort(array.ToArray()).PrintArray();
 InsertionSort20251707_6.Sort(array.ToArray()).PrintArray();
 InsertionSort20251807.Sort(array.ToArray()).PrintArray();
+InsertionSort20251907.Sort(array.ToArray()).PrintArray();
