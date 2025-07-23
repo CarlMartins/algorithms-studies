132d8a1b69e93bc3a131fad5f1c7df6fdadb9840
Carlos Martins <lemos_oliveiraa@hotmail.com>
2025-07-23 19:47:41 -0300

    [sorting] Insertion Sort

--- algorithms-studies/Program.cs
+++ algorithms-studies/Program.cs
@@ -28,6 +28,7 @@ Console.WriteLine("\n___INSERTION SORT___\n");
 
 InsertionSort20250721.Sort(array.ToArray()).PrintArray();
 InsertionSort20250722.Sort(array.ToArray()).PrintArray();
+InsertionSort20250723.Sort(array.ToArray()).PrintArray();
 
 Console.WriteLine("\n___ARRAYS - ROTATE AN ARRAY BY K ELEMENTS___\n");
 
